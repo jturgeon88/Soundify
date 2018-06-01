de5ed3a82f4ef26b57672bd739e25f548fa2bbe6
Josh Turgeon <j_turgeon88@yahoo.com>
2018-06-01 12:05:45 -0500

    playlist redux loop - ajax, actions, reducer

---
+++ frontend/actions/playlist_actions.js
@@ -0,0 +1,94 @@
+import * as PlaylistAPIUtil from '../util/playlist_util';
+
+export const RECEIVE_PLAYLISTS = "RECEIVE_PLAYLISTS";
+export const RECEIVE_PLAYLIST = "RECEIVE_PLAYLIST";
+export const REMOVE_PLAYLIST= "REMOVE_PLAYLIST";
+export const RECEIVE_PLAYLIST_ERRORS = "RECEIVE_PLAYLIST_ERRORS";
+// TODO
+// export const CLEAR_PLAYLISTS = "CLEAR_PLAYLISTS";
+// export const RECEIVE_PLAYLIST_SEARCH_RESULTS = "RECEIVE_PLAYLIST_SEARCH_RESULTS";
+// export const RECEIVE_RECENT_PLAYLISTS = "RECEIVE_RECENT_PLAYLISTS";
+
+// Synchronous action creators:
+
+const receivePlaylists = playlists => ({
+  type: RECEIVE_PLAYLISTS,
+  playlists
+});
+
+const receivePlaylist = playlist => ({
+  type: RECEIVE_PLAYLIST,
+  playlist
+});
+
+const removePlaylist = playlist => ({
+  type: REMOVE_PLAYLIST,
+  playlist
+});
+
+const receivePlaylistErrors = errors => ({
+  type: RECEIVE_PLAYLIST_ERRORS,
+  errors
+});
+
+const removePlaylists = () => ({
+  type: CLEAR_PLAYLISTS,
+  playlists: []
+});
+
+// const receivePlaylistSearchResults = searchResults => ({
+//   type: RECEIVE_PLAYLIST_SEARCH_RESULTS,
+//   searchResults
+// });
+//
+// const receiveRecentPlaylists = recentPlaylistResults => ({
+//   type: RECEIVE_RECENT_PLAYLISTS,
+//   recentPlaylistResults
+// });
+
+
+// Asynchronous action creators:
+
+export const fetchPlaylists = () => dispatch => (
+  PlaylistAPIUtil.fetchPlaylists()
+    .then(playlists => dispatch(receivePlaylists(playlists)),
+          errors => dispatch(receivePlaylistErrors(errors.responseJSON)))
+);
+
+export const fetchPlaylist = id => dispatch => (
+  PlaylistAPIUtil.fetchPlaylist(id)
+    .then(playlist => dispatch(receivePlaylist(playlist)),
+          errors => dispatch(receivePlaylistErrors(errors.responseJSON)))
+);
+
+export const createPlaylist = playlist => dispatch => (
+  PlaylistAPIUtil.createPlaylist(playlist)
+    .then(newPlaylist => dispatch(receivePlaylist(newPlaylist)),
+          errors => dispatch(receivePlaylistErrors(errors.responseJSON)))
+);
+
+export const updatePlaylist = playlist => dispatch => (
+  PlaylistAPIUtil.updatePlaylist(playlist)
+    .then(newPlaylist => dispatch(receivePlaylist(newPlaylist)),
+          errors => dispatch(receivePlaylistErrors(errors.responseJSON)))
+);
+
+export const deletePlaylist = id => dispatch => (
+  PlaylistAPIUtil.deletePlaylist(id)
+    .then(playlist => dispatch(removePlaylist(playlist)),
+          errors => dispatch(receivePlaylistErrors(errors.responseJSON)))
+);
+
+export const clearPlaylistErrors = () => dispatch => (
+  dispatch(receivePlaylistErrors([]))
+);
+
+// export const searchPlaylists = title => dispatch => (
+//   PlaylistAPIUtil.searchPlaylists(title)
+//     .then(playlists => dispatch(receivePlaylistSearchResults(playlists)))
+// );
+//
+// export const recentPlaylists = () => dispatch => (
+//   PlaylistAPIUtil.recentPlaylists()
+//     .then(serverPlaylists => dispatch(receiveRecentPlaylists(serverPlaylists)))
+// );
---
+++ frontend/reducers/entities/playlists_reducer.js
@@ -0,0 +1,52 @@
+import {
+  RECEIVE_PLAYLISTS,
+  RECEIVE_PLAYLIST,
+  REMOVE_PLAYLIST,
+  RECEIVE_PLAYLIST_ERRORS,
+} from '../../actions/playlist_actions';
+
+// import {
+//   RECEIVE_SONGS,
+//   RECEIVE_SONG,
+//   REMOVE_SONG
+// } from '../../actions/song_actions';
+
+import merge from 'lodash/merge';
+
+const playlistsReducer = (state = {}, action) => {
+  Object.freeze(state);
+  let newState;
+  // let songId, playlistId;
+
+  switch (action.type) {
+    case RECEIVE_PLAYLISTS:
+      return merge({}, action.playlists);
+    case RECEIVE_PLAYLIST:
+      return merge({}, state, action.playlist);
+    case REMOVE_PLAYLIST:
+      newState = merge({}, state);
+      delete newState[action.playlist.id];
+      return newState;
+    // case RECEIVE_SONG:
+    //   newState = merge({}, state);
+    //   songId = parseInt(Object.keys(action.song)[0]);
+    //   playlistId = action.song[songId].playlistId;
+    //   newState[playlistId].songs.push(songId);
+    //   return newState;
+    // case REMOVE_SONG:
+    //   newState = merge({}, state);
+    //   songId = action.song.id;
+    //   playlistId = action.song.playlistId;
+    //   const songArray = state[action.song.playlistId].songs;
+    //   const songIndex = songArray.indexOf(songId);
+    //   songArray.splice(songIndex, 1);
+    //   return newState;
+    default:
+      return state;
+  }
+};
+
+// TODO add cases for RECEIVE_SONG and REMOVE_SONG
+
+
+export default playlistsReducer;
