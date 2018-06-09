5f62752c5d5733a353deb7cbb3f92b659d0c4ae2
Josh Turgeon <j_turgeon88@yahoo.com>
2018-06-09 15:10:08 -0500

    songsReducer created and tested

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 
 import usersReducer from './users_reducer';
 import playlistsReducer from './playlists_reducer';
+import songsReducer from './playlists_reducer';
 
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
-  playlists: playlistsReducer
+  playlists: playlistsReducer,
+  songs: songsReducer
 });
 
 export default entitiesReducer;
