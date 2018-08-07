cbf7944cf1ec85bbd746134c9ead978a0f627736
Josh Turgeon <j_turgeon88@yahoo.com>
2018-08-07 16:05:11 -0500

    currentSong on Local state for MusicBar

--- frontend/components/music_bar/music_bar.jsx
+++ frontend/components/music_bar/music_bar.jsx
@@ -14,34 +14,34 @@ import ReactAudioPlayer from 'react-audio-player';
 
 
 class MusicBar extends React.Component {
-  // constructor (props) {
-  //   super(props);
-  //   this.state = {
-  //     currentSong: "bensound-slowmotion.mp3"
-  //   };
-  //
-  //   this.changeSong = this.changeSong.bind(this);
-  // }
+  constructor (props) {
+    super(props);
+    this.state = {
+      currentSong: this.props.nowPlayingQueue[0] ? this.props.nowPlayingQueue[0].src : "bensound-slowmotion.mp3",
+      currentSongIdx: 0
+    };
+
+    this.changeSong = this.changeSong.bind(this);
+    this.nextSong = this.nextSong.bind(this);
+  }
 
   changeSong(newSong) {
     this.setState({currentSong: newSong})
   }
 
   componentWillMount() {
-    console.log("testingWILLMOUNT");
+    // console.log("testingWILLMOUNT");
   }
 
   componentDidMount() {
-    console.log("testingDIDMOUNT");
-    // const firstIndex = this.props.playlistIndices[0];
-    // console.log(firstIndex);
-    // this.props.fetchNextPlaylist(firstIndex);  // TODO This needs to change. Can't have this hard coded id
+    // console.log("testingDIDMOUNT");
   }
 
 
-  handleClick (event) {
+  nextSong (event) {
     event.preventDefault();
-
+    const { nowPlayingQueue } = this.props;
+    this.setState({currentSong: this.props.nowPlayingQueue[(this.state.currentSongIdx + 1) % nowPlayingQueue.length].src, currentSongIdx: this.state.currentSongIdx + 1});
   }
 
 
@@ -49,11 +49,14 @@ class MusicBar extends React.Component {
     const { nowPlayingQueue } = this.props;
     if (!(nowPlayingQueue == undefined) && (nowPlayingQueue.length > 0)) {
       const randomIndex = Math.floor(Math.random() * Math.floor(nowPlayingQueue.length))
-      const nowPlayingSong = nowPlayingQueue[randomIndex];
-      console.log("Test 2 (playlist)");
-      console.log(nowPlayingQueue);
-      console.log(nowPlayingSong);
-      console.log(randomIndex);
+      // const nowPlayingSong = nowPlayingQueue[randomIndex];
+      const nowPlayingSong = this.state.currentSong;
+      // console.log("Test 2 (playlist)");
+      // console.log(nowPlayingQueue);
+      // console.log(nowPlayingSong);
+      // console.log(randomIndex);
+      console.log(this.state.currentSong);
+      console.log(this.state.currentSongIdx);
 
 
       return (
@@ -63,12 +66,13 @@ class MusicBar extends React.Component {
 
             <audio
               ref="player"
-              src={nowPlayingSong.src}
-              controls
+              src={this.state.currentSong}
+              autoPlay
             />
             <div>
               <button onClick={() => this.refs.player.play()}>Play</button>
               <button onClick={() => this.refs.player.pause()}>Pause</button>
+              <button onClick={this.nextSong}>Next Song</button>
             </div>
 
           </footer>
@@ -79,7 +83,17 @@ class MusicBar extends React.Component {
       return (
         <div className="music-bar-container">
           <footer className="music-bar-footer">
-            <h1>That didn't work!</h1>
+            <h1>Music Bar</h1>
+
+            <audio
+              ref="player"
+              src={this.state.currentSong}
+            />
+            <div>
+              <button onClick={() => this.refs.player.play()}>Play</button>
+              <button onClick={() => this.refs.player.pause()}>Pause</button>
+              <button onClick={this.nextSong}>Next Song</button>
+            </div>
 
           </footer>
         </div>
