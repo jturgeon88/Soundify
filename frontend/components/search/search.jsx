c36b042350471efc55772bd8e1ba34ce6bb98abf
Josh Turgeon <j_turgeon88@yahoo.com>
2018-09-11 21:26:30 -0500

    create search component

---
+++ frontend/components/search/search.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputVal: ''
+    }
+  }
+
+
+  render() {
+
+    return (
+
+    );
+  }
+}
